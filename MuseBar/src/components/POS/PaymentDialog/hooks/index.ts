5114012ad440c049a2bff1be78907b8b434680d0
evergne <elliot.vergne@gmail.com>
2025-09-03 15:02:02 +0200

    feat(pos): modularize usePaymentLogic into focused payment hooks; 20/28 files complete (71.4%)

---
+++ MuseBar/src/components/POS/PaymentDialog/hooks/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Payment Hooks Index
+ * Aggregates and re-exports all payment processing hooks
+ */
+
+export * from './usePaymentState';
+export * from './usePaymentCalculations';
+export * from './usePaymentValidation';
+export * from './usePaymentProcessing';
