5da122db932c0f8550ff29571c97b5d4108b02f8
evergne <elliot.vergne@gmail.com>
2025-09-03 15:06:53 +0200

    feat(admin): modularize UserManagement into focused hooks; 21/28 files complete (75%)

---
+++ MuseBar/src/components/Admin/UserManagement/hooks/index.ts
@@ -0,0 +1,9 @@
+/**
+ * User Management Hooks Index
+ * Aggregates and re-exports all user management hooks
+ */
+
+export * from './useUserState';
+export * from './useUserActions';
+export * from './usePermissions';
+export * from './useUserForm';
